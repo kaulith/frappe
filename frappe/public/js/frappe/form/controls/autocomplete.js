66062e4aff190a57290c4969ba4835ee590733a0
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-27 03:12:26 +0530

    fix: multiselect pills adds an unselected option after picking one

--- frappe/public/js/frappe/form/controls/autocomplete.js
+++ frappe/public/js/frappe/form/controls/autocomplete.js
@@ -161,7 +161,7 @@ frappe.ui.form.ControlAutocomplete = class ControlAutoComplete extends frappe.ui
 
 		options = options.map((o) => {
 			if (typeof o !== "string") {
-				o.label = __(cstr(o.label));
+				o.label = __(cstr(o.label)) || cstr(o.value);
 				o.value = cstr(o.value);
 			}
 			return o;
